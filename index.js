6aeff8be12d887e018fd74a6c7897dc2a42c560a
clearisoft <clearisoft@aliyun.com>
2020-12-16 01:29:20 +0800

    fix get rotate bug

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ module.exports = function (filename) {
     filename
   ]).then(function (out) {
     var stdout = out[0].toString('utf8');
+    stdout = stdout.replace(/["]/g, '');
     var width = /width=(\d+)/.exec(stdout);
     var height = /height=(\d+)/.exec(stdout);
-    var rotate = parseInt(/rotate=(\d+)/.exec(stdout));
-    var swap = 0 != ((rotate + 360) % 180);
+    var swap = false;
+    var rotate = /rotate=(\d+)/.exec(stdout);
+    if (rotate) {
+      swap = 0 != ((parseInt(rotate[1])+ 360) % 180);
+    }
     assert(width && height, 'No dimensions found!');
     return {
       width: parseInt(swap ? height[1] : width[1]),
